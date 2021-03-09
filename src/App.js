241f5c1e7c449827f503a314aa6fe71c0d7656eb
D-aisy <44583789+D-aisy@users.noreply.github.com>
2021-03-09 14:18:13 +0000

    imported bootstrap

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Weather App
+      <div className="container">
+      <h1>Weather App</h1>
+      <p>open sourced code by <a href="https://github.com/D-aisy/react-weather-project" target="_blank" rel="noreferrer">Daisy Cowley</a></p>
+    </div>
     </div>
   );
 }

28d384481c9d183ea5d6a4c70a9070a987d5d98f
D-aisy <44583789+D-aisy@users.noreply.github.com>
2021-03-09 16:30:37 +0000

    Styled page with CSS

--- src/Weather.js
+++ src/Weather.js
@@ -8,7 +8,7 @@ export default function Weather(){
             <form>
                 <div className="row">
                    <div className="col-9">
-                <input type="search" placeholder="Enter City Here.." className="form-control"/>
+                <input type="search" placeholder="Enter City Here.." className="form-control" autoFocus="on"/>
                 </div>
                 <div className="col-3">
                 <input type="button" value="Search" className="btn btn-light"/>
@@ -19,16 +19,19 @@ export default function Weather(){
                 <li>Tuesday, 9th March</li>
                 <li>Clear Sky</li>
             </ul>
+            <div className="clearfix">
             <div className="row">
                 <div className="col-6">
-                    <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="sunny"/>
-                    <span>6°C</span>
+                    
+                    <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="sunny" className="float-left"/>
+                    <span className="temperature">6</span><span className="units">°C</span>
                 </div>
                 <div className="col-6">
                     <ul>
                         <li>Humidity: 44%</li>
                         <li>Wind: 4 Mph</li>
                     </ul>
+                </div>
                 </div>
             </div>
         </div>

15beb29f079398d29d9423db141e0863bd38c96d
D-aisy <44583789+D-aisy@users.noreply.github.com>
2021-03-09 15:20:09 +0000

    Added fake data with JSX

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-
+import React from "react";
 import './App.css';
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-      <h1>Weather App</h1>
+      
+      <Weather />
       <p>open sourced code by <a href="https://github.com/D-aisy/react-weather-project" target="_blank" rel="noreferrer">Daisy Cowley</a></p>
     </div>
     </div>
---
+++ src/Weather.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather(){
+    return(
+        <div className="Weather">
+            <h1>Bristol</h1>
+            <form>
+                <div className="row">
+                   <div className="col-9">
+                <input type="search" placeholder="Enter City Here.." className="form-control"/>
+                </div>
+                <div className="col-3">
+                <input type="button" value="Search" className="btn btn-light"/>
+                </div>
+                </div>
+            </form>
+            <ul>
+                <li>Tuesday, 9th March</li>
+                <li>Clear Sky</li>
+            </ul>
+            <div className="row">
+                <div className="col-6">
+                    <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="sunny"/>
+                    <span>6°C</span>
+                </div>
+                <div className="col-6">
+                    <ul>
+                        <li>Humidity: 44%</li>
+                        <li>Wind: 4 Mph</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
